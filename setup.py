c815e018b5f47434c2af48a9175f4af9df102116
言葉 <funaox@gmail.com>
2022-08-08 16:31:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='win11toast',
-    version='0.9',
+    version='0.10',
     description='Toast notifications for Windows 10 and 11',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
